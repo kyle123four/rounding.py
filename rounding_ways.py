552f230ca333a732dd1a3f7750e0a79eba8fee94
kyle123four <kylew.23137@obhs.school.nz>
2025-08-21 09:08:10 +1200

    round
    Z

---
+++ rounding_ways.py
@@ -0,0 +1,7 @@
+ 
+
+total = (10.2823 + 7.6991)/ 2.66
+print(f'This is the total]{total:2f}')        #printing as a flaot number
+print('This is the total]',round(total,2)) 
+#string formatting with slicng
+
